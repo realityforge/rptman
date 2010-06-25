b843095e58286ba16cc8dd49389d7ef57287406f
Peter Donald <peter@realityforge.org>
2010-06-25 11:39:25 +1000

    Refactor the uploader to use the new Config class

--- lib/ssrs/uploader.rb
+++ lib/ssrs/uploader.rb
@@ -1,38 +1,32 @@
 module SSRS
   class Uploader
-    def initialize
-      wsdl_url = Java::JavaNet.URL.new(SSRS.ssrs_config[SSRS::WSDL_KEY])
-      prefix = SSRS.ssrs_config[SSRS::PREFIX_KEY] 
-      @ssrs = Java::IrisSSRS::SSRS.new(wsdl_url, prefix)
-    end
-
-    def upload
-      self.upload_datasources
-      self.upload_reports
+    def self.upload
+      ssrs_soap_port = Java::IrisSSRS::SSRS.new(Java::JavaNet.URL.new(SSRS::Config.wsdl_path),
+                                                SSRS::Config.upload_prefix)
+      self.upload_datasources(ssrs_soap_port)
+      self.upload_reports(ssrs_soap_port)
     end
 
     private
 
-    def upload_datasources
-      @ssrs.mkdir( SSRS::DataSource::BASE_PATH )
-      SSRS.datasources.each do |ds|
+    def self.upload_datasources(ssrs_soap_port)
+      ssrs_soap_port.mkdir(SSRS::DataSource::BASE_PATH)
+      SSRS::Config.datasources.each do |ds|
         SSRS.info("Creating DataSource #{ds.name}")
-        @ssrs.delete( ds.symbolic_name )
-        @ssrs.createSQLDataSource( ds.symbolic_name, ds.connection_string )
+        ssrs_soap_port.delete(ds.symbolic_name)
+        ssrs_soap_port.createSQLDataSource(ds.symbolic_name, ds.connection_string)
       end
     end
 
-    def upload_reports
-      SSRS.report_dirs.each do |report_dir|
-        upload_dir = SSRS.upload_path(report_dir).split('/').delete_if{|path| path == ""}.first
-        @ssrs.delete( upload_dir )
+    def self.upload_reports(ssrs_soap_port)
+      SSRS::Config.upload_dirs.each do |report_dir|
+        upload_dir = report_dir.split('/').delete_if { |path| path == "" }.first
+        ssrs_soap_port.delete(upload_dir)
       end
-      SSRS.reports.each do |report|
-        upload_path = SSRS.upload_path(report.filename)
-        SSRS.info("Uploading #{upload_path} from #{report.filename}")
-        @ssrs.mkdir( File.dirname(upload_path) )
-        @ssrs.createReport( "#{File.dirname(upload_path)}/#{File.basename(upload_path,'.rdl')}",
-                            Java::JavaIo.File.new(report.generate_upload_version) )
+      SSRS::Config.reports.each do |report|
+        SSRS.info("Uploading #{report.name} from #{report.filename}")
+        ssrs_soap_port.mkdir(File.dirname(report.name))
+        ssrs_soap_port.createReport(report.name, Java::JavaIo.File.new(report.generate_upload_version))
       end
     end
   end
