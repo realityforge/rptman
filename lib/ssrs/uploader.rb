e7708935088290e1897670149890f98390ad1620
Peter Donald <peter@realityforge.org>
2010-06-25 15:47:39 +1000

    Remove some log output that is duplicated in java code

--- lib/ssrs/uploader.rb
+++ lib/ssrs/uploader.rb
@@ -12,7 +12,6 @@ module SSRS
     def self.upload_datasources(ssrs_soap_port)
       ssrs_soap_port.mkdir(SSRS::DataSource::BASE_PATH)
       SSRS::Config.datasources.each do |ds|
-        SSRS.info("Creating DataSource #{ds.name}")
         ssrs_soap_port.delete(ds.symbolic_name)
         ssrs_soap_port.createSQLDataSource(ds.symbolic_name, ds.connection_string)
       end
@@ -25,7 +24,6 @@ module SSRS
         ssrs_soap_port.delete(upload_dir)
       end
       SSRS::Config.reports.each do |report|
-        SSRS.info("Uploading #{report.name} from #{report.filename}")
         ssrs_soap_port.mkdir(File.dirname(report.name))
         ssrs_soap_port.createReport(report.name, Java::JavaIo.File.new(report.generate_upload_version))
       end
