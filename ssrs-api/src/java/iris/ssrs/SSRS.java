8a0ef0b6b59ded23bd779f91f96305e13c4b93a0
Peter Donald <peter@realityforge.org>
2010-07-08 14:51:50 +1000

    Remove unused import

--- ssrs-api/src/java/iris/ssrs/SSRS.java
+++ ssrs-api/src/java/iris/ssrs/SSRS.java
@@ -18,7 +18,6 @@ import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 import javax.xml.namespace.QName;
 
 /**
