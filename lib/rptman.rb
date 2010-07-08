80894f7e01ce660c198e559034043ea2d5f721db
Peter Donald <peter@realityforge.org>
2010-07-08 10:05:03 +1000

    Make sure paths are fully expanded prior to require

--- lib/rptman.rb
+++ lib/rptman.rb
@@ -2,13 +2,13 @@ require 'erb'
 require 'yaml'
 require 'optparse'
 
-require "#{File.dirname(__FILE__)}/ssrs/ssrs-api.jar"
-require "#{File.dirname(__FILE__)}/ssrs/UUID.rb"
-require "#{File.dirname(__FILE__)}/ssrs/core.rb"
-require "#{File.dirname(__FILE__)}/ssrs/config.rb"
-require "#{File.dirname(__FILE__)}/ssrs/datasource.rb"
-require "#{File.dirname(__FILE__)}/ssrs/report_project.rb"
-require "#{File.dirname(__FILE__)}/ssrs/report.rb"
-require "#{File.dirname(__FILE__)}/ssrs/uploader.rb"
-require "#{File.dirname(__FILE__)}/ssrs/bids.rb"
-require "#{File.dirname(__FILE__)}/ssrs/shell.rb"
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/ssrs-api.jar")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/UUID.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/core.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/config.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/datasource.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/report_project.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/report.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/uploader.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/bids.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/ssrs/shell.rb")
