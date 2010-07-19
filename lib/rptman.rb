d5af5c5b3557f6f464a8659de2bb944a951d5db8
Peter Donald <peter@realityforge.org>
2010-07-19 17:48:30 +1000

    Don't force absolute paths. Instead assume files are on the LOAD_PATH

--- lib/rptman.rb
+++ lib/rptman.rb
@@ -2,13 +2,13 @@ require 'erb'
 require 'yaml'
 require 'optparse'
 
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/ssrs-api.jar")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/UUID.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/core.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/config.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/datasource.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/report_project.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/report.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/uploader.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/bids.rb")
-require File.expand_path("#{File.dirname(__FILE__)}/ssrs/shell.rb")
+require 'ssrs/ssrs-api.jar'
+require 'ssrs/UUID'
+require 'ssrs/core'
+require 'ssrs/config'
+require 'ssrs/datasource'
+require 'ssrs/report_project'
+require 'ssrs/report'
+require 'ssrs/uploader'
+require 'ssrs/bids'
+require 'ssrs/shell'
