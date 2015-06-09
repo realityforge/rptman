55ffc1a42a9ef94a13509d432b826f320a98332d
Peter Donald <peter@realityforge.org>
2015-06-09 17:02:00 +1000

    Import code changes from master branch

--- lib/ssrs/uploader.rb
+++ lib/ssrs/uploader.rb
@@ -1,19 +1,34 @@
 module SSRS
   class Uploader
     def self.upload
-      # If domain has been specified then assume NTLM
-      if SSRS::Config.username
-        Java::IrisSSRS::NTLMAuthenticator.install(SSRS::Config.domain,
-                                                  SSRS::Config.username,
-                                                  SSRS::Config.password)
-      end
-      ssrs_soap_port = Java::IrisSSRS::SSRS.new(Java::JavaNet.URL.new(SSRS::Config.wsdl_path),
-                                                SSRS::Config.upload_prefix)
+      ssrs_soap_port = create_port
       self.upload_datasources(ssrs_soap_port)
       self.upload_reports(ssrs_soap_port)
     end
 
+    def self.delete
+      ssrs_soap_port = create_port
+      self.delete_datasources(ssrs_soap_port)
+      self.delete_reports(ssrs_soap_port)
+    end
+
     private
+
+    def self.create_port
+      # If domain has been specified then assume NTLM
+      if SSRS::Config.username
+        Java.iris.ssrs.NTLMAuthenticator.install(SSRS::Config.domain,
+                                                  SSRS::Config.username,
+                                                  SSRS::Config.password)
+      end
+      Java.iris.ssrs.SSRS.new(Java.java.net.URL.new(SSRS::Config.wsdl_path), SSRS::Config.upload_prefix)
+    end
+
+    def self.delete_datasources(ssrs_soap_port)
+      SSRS::Config.datasources.each do |ds|
+        ssrs_soap_port.delete(ds.symbolic_name)
+      end
+    end
 
     def self.upload_datasources(ssrs_soap_port)
       ssrs_soap_port.mkdir(SSRS::DataSource::BASE_PATH)
@@ -23,15 +38,23 @@ module SSRS
       end
     end
 
+    def self.delete_reports(ssrs_soap_port)
+      top_level_upload_dirs =
+        SSRS::Config.upload_dirs.collect { |d| d.split('/').delete_if { |p| p == '' }.first }.sort.uniq
+      top_level_upload_dirs.each do |upload_dir|
+        ssrs_soap_port.delete(upload_dir)
+      end
+    end
+
     def self.upload_reports(ssrs_soap_port)
       top_level_upload_dirs =
-        SSRS::Config.upload_dirs.collect { |d| d.split('/').delete_if { |p| p == "" }.first }.sort.uniq
+        SSRS::Config.upload_dirs.collect { |d| d.split('/').delete_if { |p| p == '' }.first }.sort.uniq
       top_level_upload_dirs.each do |upload_dir|
         ssrs_soap_port.delete(upload_dir)
       end
       SSRS::Config.reports.each do |report|
         ssrs_soap_port.mkdir(File.dirname(report.name))
-        ssrs_soap_port.createReport(report.name, Java::JavaIo.File.new(report.generate_upload_version))
+        ssrs_soap_port.createReport(report.name, Java.java.io.File.new(report.generate_upload_version))
       end
     end
   end
